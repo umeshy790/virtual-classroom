78e3f5b53ebb8b336345117deec49b29b4a5021e
Umesh <umeshy790@gmail.com>
2020-01-19 18:23:08 +0530

    Updates

---
+++ precache-manifest.f3eedabd7d89102db9c9b0ac79c87666.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dfae9afe8356e4c42de63c51f7a5d22",
+    "url": "/virtual-classroom/index.html"
+  },
+  {
+    "revision": "2b54a409c32551da6e98",
+    "url": "/virtual-classroom/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "83e8cf22a2ffb7ada3c3",
+    "url": "/virtual-classroom/static/js/2.3aae07e2.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/virtual-classroom/static/js/2.3aae07e2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2b54a409c32551da6e98",
+    "url": "/virtual-classroom/static/js/main.83fc27fc.chunk.js"
+  },
+  {
+    "revision": "0ad3d839f7dd171d7acb",
+    "url": "/virtual-classroom/static/js/runtime-main.7e00e867.js"
+  },
+  {
+    "revision": "49d4041b8e7e375f3e2950a9738bc59b",
+    "url": "/virtual-classroom/static/media/pdf.49d4041b.png"
+  }
+]);
